7cb491cbf9b2fe358ce37c30e1e813155f891e0d
Juan V <valentin.juan05@gmail.com>
2023-07-24 19:32:07 -0300

    Add withDraw method

--- src/Conta.java
+++ src/Conta.java
@@ -8,4 +8,14 @@ public class Conta {
         this.saldo += valor;
     }
 
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -2,8 +2,11 @@ public class TestaMetodo {
     public static void main(String[] args) {
         Conta conta = new Conta();
         conta.saldo = 100;
-
         conta.deposita(50);
-        System.out.println(conta.saldo);
+        System.out.println("Saldo da conta depois do deposito: " + conta.saldo);
+
+        boolean conseguiuRetirar = conta.saca(20);
+        System.out.println("Saldo atualizado:" + conta.saldo);
+        System.out.println(conseguiuRetirar);
     }
 }
